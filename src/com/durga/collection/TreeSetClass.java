907d65238cb309f7c431a6fe31e19e37276b6586
Sinha18 <sinhaanurag18@gmail.com>
2018-09-21 15:06:08 +0530

    comparator example

--- src/com/durga/collection/TreeSetClass.java
+++ src/com/durga/collection/TreeSetClass.java
@@ -1,16 +1,37 @@
 package com.durga.collection;
 
+import java.util.Comparator;
 import java.util.TreeSet;
 
 public class TreeSetClass {
 	public static void main(String args[]){
-		TreeSet t=new TreeSet();
-		t.add("A");
-		t.add("Z");
-		t.add("a");
-		t.add("z");
-		t.add(null);
+		TreeSet t=new TreeSet(new MyComparator());
+		t.add(10);
+		t.add(20);
+		t.add(0);
+		t.add(9);
+		t.add(2);
 		System.out.println("TreeSet Content:"+t);
 	}
 
+}
+class MyComparator implements Comparator{
+
+	@Override
+	public int compare(Object o1, Object o2) {
+		// TODO Auto-generated method stub
+		Integer i=(Integer)o1;
+		Integer j=(Integer)o2;
+			if(i>j){
+				return -1;
+			}
+			else
+				if(i<j)
+				{
+					return 1;
+				}
+				else 
+					return 0;
+		}
+	
 }
